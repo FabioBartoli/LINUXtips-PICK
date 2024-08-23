47ab6573ee121e289e4302311c738e6ba8d7e549
Fabio Bartoli <fabio.bartoli@outlook.com>
2024-08-23 16:14:05 -0300

    remove kyverno

--- modules/k8s_provisioner/main.tf
+++ modules/k8s_provisioner/main.tf
@@ -276,10 +276,6 @@ resource "aws_instance" "worker" {
         curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
         chmod 700 get_helm.sh
         ./get_helm.sh
-        # Kyverno
-        helm repo add kyverno https://kyverno.github.io/kyverno/
-        helm repo update
-        helm install kyverno kyverno/kyverno --namespace kyverno --create-namespace
       fi
       EOF
     ]
