25403ca8e5280c9f968e61753c9d5a4648644ca0
Fabio Bartoli <fabio.bartoli@outlook.com>
2024-08-25 20:58:24 -0300

    adjusts in general

--- scripts/install-helms.sh
+++ scripts/install-helms.sh
@@ -2,16 +2,16 @@
 
 # Instalando o Harbor
 kubectl config set-context --current --namespace=harbor
-helm install harbor harbor/harbor --set expose.type=clusterIP --set expose.tls.auto.commonName=fabiobartoli.com.br --set persistence.enabled=false --set externalURL=https://harbor.fabiobartoli.com.br
+helm install harbor harbor/harbor --set expose.type=clusterIP --set expose.tls.auto.commonName=fabiobartoli.com.br --set persistence.enabled=false --set externalURL=https://harbor.fabiobartoli.com.br --set fullnameOverride=harbor-harbor
 # Instalando o Kube-Prometheus
 kubectl config set-context --current --namespace=monitoring
 helm install kube-prometheus prometheus-community/kube-prometheus-stack
 # Instalando o Kyverno
 kubectl config set-context --current --namespace=default
-helm install kyverno kyverno/kyverno --namespace kyverno --create-namespace
+helm install kyverno kyverno/kyverno --namespace kyverno --create-namespace --version 2.6.5
 #### Instalando o Locust
 sudo mkdir -p /usr/src/app/scripts/
-sudo mv ../manifests/locust/locustfile.py /usr/src/app/scripts/locustfile.py
-kubectl apply -f ../manifests/locust/
+sudo mv /home/ubuntu/LINUXtips-PICK/manifests/locust/locustfile.py /usr/src/app/scripts/locustfile.py
+kubectl apply -f /home/ubuntu/LINUXtips-PICK/manifests/locust/
 ###### Instalando os Ingress
-kubectl apply -f ../manifests/ingress/
+kubectl apply -f /home/ubuntu/LINUXtips-PICK/manifests/ingress/
